7cac456f9e23dfbcbfb7190fde160c0293eabe02
Marcono1234 <Marcono1234@users.noreply.github.com>
2025-12-23 21:00:27 +0100

    Fix outdated Foojay resolver plugin for downloading JDKs
    If the JDK was missing, the plugin failed for Gradle version 9, which
    is used by the Gradle Wrapper.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,6 @@ rootProject.name = "jtreesitter-type-gen"
 include("lib", "cli")
 
 plugins {
-    // For resolving JDK toolchains, see https://docs.gradle.org/8.11/userguide/toolchains.html#sec:provisioning
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+    // For resolving JDK toolchains, see https://docs.gradle.org/9.1.0/userguide/toolchains.html#sec:provisioning
+    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
 }
